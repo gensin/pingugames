35a53f34222401bed0d15d3ea8a6c0ce6d90ddf1
Álvaro <lvaro@AlvarOS>
2015-01-03 14:34:24 +0100

    classes Card & Constants

---
+++ src/main/java/org/pingus/model/Card.java
@@ -0,0 +1,33 @@
+package main.java.org.pingus.model;
+
+public class Card {
+
+	private Integer idCard;
+	private Integer numCard;
+	private String clubCard;
+	private String kindCard;
+	
+	public Card(Integer idCard,Integer numCard,String clubCard,String kindCard){
+		this.idCard=idCard;
+		this.numCard=numCard;
+		this.clubCard=clubCard;
+		this.kindCard=kindCard;
+	}
+
+	public Integer getIdCard() {
+		return idCard;
+	}
+
+	public Integer getNumCard() {
+		return numCard;
+	}
+
+	public String getClubCard() {
+		return clubCard;
+	}
+
+	public String getKindCard() {
+		return kindCard;
+	}
+	
+}
---
+++ src/main/java/org/pingus/utils/Constants.java
@@ -0,0 +1,17 @@
+package main.java.org.pingus.utils;
+
+public class Constants {
+
+	public static String SPA_SWORDS = "Swords";
+	public static String SPA_GOLDS = "Golds";
+	public static String SPA_CLUBS = "Clubs";
+	public static String SPA_CUPS = "Cups";
+	public static String SPA_ACE = "Ace";
+	public static String SPA_JACK = "Jack";
+	public static String SPA_KNIGHT = "Knight";
+	public static String SPA_KING = "King";
+	
+	public static Integer SPA_CLUBLENGTH = 12;
+	public static Integer SPA_DECKLENGTH = 48;
+	public static Integer SPA_TYPE = 1;
+}
