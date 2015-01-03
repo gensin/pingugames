ddf6aee81d4845dd10962b656065d0de5fbd5166
Álvaro <lvaro@AlvarOS>
2015-01-03 15:57:31 +0100

    ESPACIOS PARA EL PIJO DE ALEX

--- src/main/java/org/pingus/model/Card.java
+++ src/main/java/org/pingus/model/Card.java
@@ -9,31 +9,32 @@ public class Card {
 	private int isJoker;
 	private String cardClub;
 	private String cardKind;
-	
-	public Card(int cardId,int cardNumber,String cardClub,String cardKind){
-		this.cardId=cardId;
-		this.cardNumber=cardNumber;
-		this.cardClub=cardClub;
-		this.cardKind=cardKind;
-		this.isJoker=Constants.NO_JOKER;
+
+	public Card(int cardId, int cardNumber, String cardClub, String cardKind) {
+		this.cardId = cardId;
+		this.cardNumber = cardNumber;
+		this.cardClub = cardClub;
+		this.cardKind = cardKind;
+		this.isJoker = Constants.NO_JOKER;
 	}
-	
-	public Card(int cardId,int cardNumber,String cardClub,String cardKind, int isJoker){
-		this.cardId=cardId;
-		this.cardNumber=cardNumber;
-		this.cardClub=cardClub;
-		this.cardKind=cardKind;
-		this.isJoker=isJoker;
+
+	public Card(int cardId, int cardNumber, String cardClub, String cardKind,
+			int isJoker) {
+		this.cardId = cardId;
+		this.cardNumber = cardNumber;
+		this.cardClub = cardClub;
+		this.cardKind = cardKind;
+		this.isJoker = isJoker;
 	}
-	
-	public int addValuesJoker (int cardNumber, String cardClub, String cardKind){
-		if(this.isJoker==Constants.NO_JOKER){
-			this.isJoker=Constants.JOKER;
+
+	public int addValuesJoker(int cardNumber, String cardClub, String cardKind) {
+		if (this.isJoker == Constants.NO_JOKER) {
+			this.isJoker = Constants.JOKER;
 		}
-			this.cardNumber=cardNumber;
-			this.cardClub=cardClub;
-			this.cardKind=cardKind;
-			return this.cardId;
+		this.cardNumber = cardNumber;
+		this.cardClub = cardClub;
+		this.cardKind = cardKind;
+		return this.cardId;
 	}
 
 	public int getCardId() {
@@ -55,5 +56,5 @@ public class Card {
 	public String getCardKind() {
 		return cardKind;
 	}
-	
+
 }
